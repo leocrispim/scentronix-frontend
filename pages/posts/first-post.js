676094dceb7194f2a94b18e31f71ee898715968e
User <leonardo.crispim@outlook.com.br>
2021-02-11 21:55:38 -0300

    First page initial concept

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,3 +1,6 @@
+import * as SC from '../../Styles/styledcomponents'
+import React from 'react';
+import Button from '@material-ui/core/Button';
 import Head from 'next/head'
 import Link from 'next/link'
 import Image from 'next/image'
@@ -6,20 +9,45 @@ export default function FirstPost() {
   return (
     <>
       <Head>
-        <title>First Post</title>
+        <title>First Page</title>
       </Head>
-      <h1>First Post</h1>
-      <h2>
-        <Link href="/">
-          <a>Back to home</a>
-        </Link>
-      </h2>
-      <Image
+      
+      <SC.HeaderContainer>
+        <Button size="large">SHOP</Button>
+        <Button size="large" variant="outlined" >RECIPES</Button>
+        <Button size="large">LEARN</Button>
+        <Button size="large">ABOUT</Button>
+        <Button size="large">BLOG</Button>
+      </SC.HeaderContainer>
+
+      <SC.CategoryContainer>
+        <SC.MealLogo src="/images/meal.png" />
+        <Button size="large">CATEGORIES</Button>
+        <Button size="large">COLLECTIONS</Button>
+        <Button size="large">RESOURCES</Button>
+      </SC.CategoryContainer>
+
+      <SC.MainContentContainer>
+        <SC.ContentLeft>
+          <p>Hello There!</p>
+        </SC.ContentLeft>
+
+        <SC.ContentRight>
+          <p>General Kenobi</p>
+        </SC.ContentRight>
+      </SC.MainContentContainer>
+
+      {/* <Image
         src="/images/eso.jpg" // Route of the image file
         height={800} // Desired size with correct aspect ratio
         width={1024} // Desired size with correct aspect ratio
         alt="Cool Wallpaper"
-      />
+      /> */}
+            <h2>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </h2>
     </>
   )
 }
